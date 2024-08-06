83479a163fc2a052da2dcf06941147abfcf8b5e3
ariella08613 <ariella.souza@escola.pr.gov.br>
2024-08-06 11:39:04 -0300

    primeiro commit

---
+++ src/main/java/com/eleodoro/biblioteca_eleodoro/BibliotecaEleodoroApplication.java
@@ -0,0 +1,15 @@
+package com.eleodoro.biblioteca_eleodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class BibliotecaEleodoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaEleodoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eleodoro/biblioteca_eleodoro/BibliotecaEleodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eleodoro.biblioteca_eleodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaEleodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
